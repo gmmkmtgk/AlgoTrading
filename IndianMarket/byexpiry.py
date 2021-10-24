3b91f153f1b83ed08580a79194ae01673cb7b4b1
gmmkmtgk <51493190+gmmkmtgk@users.noreply.github.com>
2021-10-25 00:53:48 +0530

    Add files via upload

---
+++ IndianMarket/byexpiry.py
@@ -0,0 +1,12 @@
+from nsepy import get_history
+from datetime import date
+from nsepy.derivatives import get_expiry_date
+
+expiry = get_expiry_date(year=2021, month=10)
+
+
+stock_opt = get_history(symbol="SBIN",
+                            start=date(2021,10,1),
+                            end=date(2021,10,2),
+                            futures=True,
+                            expiry_date=get_expiry_date(2021,10,10))
